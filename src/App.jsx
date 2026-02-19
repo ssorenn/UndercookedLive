e7a335cff769532575f1a1a4aca0f4e86807e91b
mia-oww <miafendi01@gmail.com>
2026-02-18 22:02:02 -0500

    add settings button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import GameScreen from "./components/GameScreen";
 import homescreenImg from "./assets/homescreen.jpg";
 import playBtnImg from "./assets/play_button.png";
+import settingsBtnImg from "./assets/settings_button.png";
 
 export default function App() {
   const [screen, setScreen] = useState("home");
@@ -20,7 +21,7 @@ export default function App() {
         height: "100%",
         objectFit: "cover",
         objectPosition: "center",
-        background: "#000",
+        background: "#fff8dc", 
       }}
     />
     <img
@@ -36,6 +37,20 @@ export default function App() {
         width: "20vw", // this is so the button is responsive and scales with the screen size, but we can adjust as needed
         //minWidth: "300px", 
         //maxWidth: "600px",  
+        cursor: "pointer",
+        zIndex: 1,
+        transition: "transform 0.15s ease",
+      }}
+    />
+    <img 
+      src={settingsBtnImg}
+      onMouseEnter={e => e.currentTarget.style.transform = " scale(1.05)"}
+      onMouseLeave={e => e.currentTarget.style.transform = " scale(1)"}
+      style={{
+        position: "absolute",
+        bottom: "10%",
+        left: "63%",
+        width: "15vw",
         cursor: "pointer",
         zIndex: 1,
         transition: "transform 0.15s ease",
